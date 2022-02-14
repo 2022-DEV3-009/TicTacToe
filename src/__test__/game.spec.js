246d2021d56f3675ec4187bfa2bfc85c016e9c9c
Massimo Regaglia <m.regaglia@gmail.com>
2022-02-14 22:57:59 +0100

    initialize tests

---
+++ src/__test__/game.spec.js
@@ -0,0 +1,39 @@
+it('should display grid with initial cells state', () => {
+  throw new Error('Not implemented');
+});
+
+it('should display X at the first click', () => {
+  throw new Error('Not implemented');
+});
+
+it('should switch between X and O at each click', () => {
+  throw new Error('Not implemented');
+});
+
+it ('should be X as winner', () => {
+  throw new Error('Not implemented');
+});
+
+it ('should be O as winner', () => {
+  throw new Error('Not implemented');
+});
+
+
+it('should be a draw', () => {
+  throw new Error('Not implemented');
+});
+
+
+it('should not be allowed to click on a filled cell', () => {
+  throw new Error('Not implemented');
+});
+
+
+it('should not be allowed to click after win', () => {
+  throw new Error('Not implemented');
+});
+
+
+it('should not be allowed to click after a draw', () => {
+  throw new Error('Not implemented');
+});
